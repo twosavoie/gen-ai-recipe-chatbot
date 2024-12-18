37ccecc1ace9e24c4134268e29acbc857ffb80f5
Vincent Thomasino <vincent.thomasino@karykeion.com>
2024-12-18 10:45:47 -0800

    eliminate download of pg data

--- gutenberg/pg_create_cache.py
+++ gutenberg/pg_create_cache.py
@@ -1,3 +1,2 @@
 from gutenbergpy.gutenbergcache import GutenbergCache
-#GutenbergCache.create(refresh=False, download=False, unpack=False, parse=True, cache=True, deleteTemp=False)
-GutenbergCache.create()
+GutenbergCache.create(refresh=False, download=False, unpack=False, parse=True, cache=True, deleteTemp=False)
