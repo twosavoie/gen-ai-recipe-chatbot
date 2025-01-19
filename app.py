7c9d2910aa65d4bfb5d85b9a906edbd79adbfe9a
Lisa Savoie <twosavoie@gmail.com>
2025-01-18 18:26:57 -0600

    Updates to templates, app.py, and README

--- app.py
+++ app.py
@@ -17,18 +17,19 @@ from openai import OpenAI
 
 client = OpenAI()
 
+# Flask app setup
 app = Flask(__name__)
-
-# Function to add to the log in the app.log file
-def log_run(run_status):
-    if run_status in ["cancelled", "failed", "expired"]:
-        log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
 
 # Render the HTML template - we're going to see a UI!!!
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+  
+# Function to add to the log in the app.log file
+def log_run(run_status):
+    if run_status in ["cancelled", "failed", "expired"]:
+        log.error(str(datetime.datetime.now()) + " Run " + run_status + "\n")
 
 # Run the Flask server
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
