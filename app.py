cb0367f6e14b6594012a83f8cb60ee893b396c03
Lisa Savoie <twosavoie@gmail.com>
2025-02-18 17:45:36 -0600

    Update to match starter

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ def my_account():
         current_user.password = generate_password_hash(new_password, method="pbkdf2:sha256")
         db.session.commit()
         flash("Password updated successfully!", "success")
-        return redirect(url_for("my_account"))
+        return redirect(url_for("index"))
 
     return render_template("my_account.html", user=current_user)
 
